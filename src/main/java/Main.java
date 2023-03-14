c542c1270c3e5d1f2a5f7236009f20b12bf2ef8e
Igor Avdonin <6516980+thathorizon@users.noreply.github.com>
2023-03-14 17:45:26 +0300

    Revert "pull request"

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,53 +1,8 @@
-dev branch for Y.Practicum
-import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("На скольких человек необходимо разделить счёт?");
-        Scanner scan = new Scanner(System.in);
-        int AmountOfPeople = scan.hasNextInt();
-        String AllNames = "";
-        double AllPrices = 0;
-
-        while (AmountOfPeople == 1 || AmountOfPeople < 1) {
-            System.out.println("Введеное значение некоректно.");
-            AmountOfPeople = scan.hasNextInt();
-        }
-        while (true) {
-            System.out.println("Напишите название товара:");
-            String name = scan.nextLine();
-            AllNames += name + " ";
-
-            System.out.println("Напишите цену товара:");
-            double price = scan.nextDouble();
-            AllPrices += price;
-            System.out.println("Хотите добавить ещё товар?");
-            String answer = scan.next();
-            if (answer.equals("Завершить")) {
-                break;
-            }
-        }
-        System.out.println("Добавленные товары: " + AllNames);
-        String prices = String.format("%.2f",AllPrices);
-        System.out.println("Итоговый счёт: " + prices);
-        double total = AllPrices / AmountOfPeople;
-        String result = String.format("%.2f",total);
-        System.out.println("Итоговая сумма с каждого гостя: " + result + " " + Formarter(total));
-    }
-    public static String Formarter(double x) {
-        
-        if(x % 100 >= 11 && x % 100 <= 19) {
-            return "рублей";
-        }
-
-        else if(x % 10 == 1) {
-            return "рубль";
-        }
-        else if (x % 10 == 2 || x % 10 == 3 || x % 10 == 4) {
-            return "рубля";
-        } else {
-            return "рублей";
-        }
-
+        // ваш код начнется здесь
+        // вы не должны ограничиваться только классом Main и можете создавать свои классы по необходимости
+        System.out.println("Привет Мир");
     }
 }
